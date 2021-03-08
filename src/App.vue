0a91c3604a23cf7fa38786875e43c1bc8af3f867
FirinKinuo <firinkinuo@protonmail.ch>
2021-03-08 18:17:43 +0300

    add header menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app" class="container-full">
+    <MainMenu></MainMenu>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainMenu from "@/components/mainMenu";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainMenu
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+:root {
+  font-size: 16pt;
+  --primary: #e71d36;
+  --secondary: #e500a4;
+  --success: #57cc99;
+  --warning: #ff9f1c;
+  --dark: #011627;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  outline: none;
+  border: 0;
+  font-family: 'Nunito', sans-serif;
+}
+
+.container-full{
+  width: 100%;
 }
 </style>
